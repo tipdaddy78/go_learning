8de3a76c1fbf1f46948ab37a126afa80c40a9f24
Tipper <tipper.langseth@gmail.com>
2021-07-15 16:45:50 -0500

    adding errors

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"github.com/tipdaddy78/go_learning/tree/master/greetings"
 )
 
 func main() {
-    message := greetings.Hello("Tipper")
+	//Set properties of logger including
+	// the log entry prefix
+	// flag to diable printing
+	// time, source file, and line number
+	log.SetPrefix("greetings: ")
+	logs.SetFlags(0)
+
+	//Request a message
+    message, err := greetings.Hello("")
+	//If an error was returned print it to console and exist
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//If no error, print message to console.
 	fmt.Println(message)	
 }
